d2e7132e27d2e739a4ab4b9d6812cc31b415d493
Luke C. J. Currie <lukecurrie12@augustana.edu>
2015-08-28 12:45:38 -0500

    Start the new Yak application and print bye.

--- TestProject/src/TestMain.java
+++ TestProject/src/TestMain.java
@@ -2,7 +2,8 @@
 public class TestMain {
 
 	public static void main(String[] args) {
-		System.out.print("Hello Emu World");
+		System.out.println("Hello Emu World");
+		System.out.println("Goodbye!");
 
 	}
 
